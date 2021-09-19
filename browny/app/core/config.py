14ec38d5355efa1a8973815b48f9e4f38e4ee74a
Youngiyong <youn9354@naver.com>
2021-09-19 16:09:02 +0900

    initial commit

---
+++ browny/app/core/config.py
@@ -0,0 +1,15 @@
+import secrets
+
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api/v1"
+    PROJECT_NAME: str = "browny"
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+    SQLALCHEMY_DATABASE_URI: str = ""
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
